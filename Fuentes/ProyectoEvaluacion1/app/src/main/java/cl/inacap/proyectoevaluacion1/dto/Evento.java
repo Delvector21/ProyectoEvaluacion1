2f7c20e850f9e919c4fd0f0cd64384a9f8934a9a
pelonchaz <kami-sama_no_kuso@live.cl>
2020-09-28 19:38:57 -0300

    agregado dto con evento, cambios en mainactivity y colores extra en colors
    Co-Authored-By: Delvector21 <69976277+Delvector21@users.noreply.github.com>

---
+++ Fuentes/ProyectoEvaluacion1/app/src/main/java/cl/inacap/proyectoevaluacion1/dto/Evento.java
@@ -0,0 +1,52 @@
+package cl.inacap.proyectoevaluacion1.dto;
+
+import java.util.Date;
+
+public class Evento {
+
+    private String nombre;
+    private String fecha;
+    private String genero;
+    private int valor;
+    private int calificacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+}
